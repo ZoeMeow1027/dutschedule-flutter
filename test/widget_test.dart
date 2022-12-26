ef6f23d8f013233d30efeefead3388ae484b2915
Quế Tùng <47247560+ZoeMeow5466@users.noreply.github.com>
2022-12-26 10:25:55 +0700

    Inital project with version 0.0.1. Nothing in this

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:subjectnotifier/main.dart';
+import 'package:subject_notifier_flutter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
