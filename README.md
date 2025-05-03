1330807fa91162a42fa2ec2c5933e8cead56bf7e
Quế Tùng <47247560+ZoeMeow1027@users.noreply.github.com>
2025-05-03 14:30:42 +0700

    v0.5.5+250503 - Minor updates
    - Prepare for removing main_view_model.dart.
    - Prepare for News tab with 5 tabs.
    - Updated dependencies to latest version.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 - Continuing from [DutSchedule](https://github.com/ZoeMeow1027/DutSchedule), but written with Dart/Flutter.
 
 ## Version
-- You're viewing v0.5.4+250413
+- You're viewing v0.5.5+250503
 - Latest release version [![https://github.com/ZoeMeow1027/dutschedule-flutter](https://img.shields.io/github/v/release/ZoeMeow1027/dutschedule-flutter)](https://github.com/ZoeMeow1027/dutschedule-flutter/releases)
 - [Entire source code changes](https://github.com/ZoeMeow1027/dutschedule-flutter/commits)
 <!--[Summary change log](CHANGELOG.md) / -->
--- lib/viewmodel/main_view_model.dart
+++ lib/viewmodel/main_view_model.dart
@@ -1,7 +1,3 @@
-import 'package:dutwrapper/news_object.dart';
-import 'package:flutter/material.dart';
-
-import '../model/enum/news_tab_location.dart';
 import 'base_view_model.dart';
 
 class MainViewModel extends BaseViewModel {
@@ -10,35 +6,6 @@ class MainViewModel extends BaseViewModel {
 
   @override
   void timerAction() {}
-
-  // News page
-  NewsTabLocation newsCurrentPage = NewsTabLocation.globalNews;
-  final PageController newsPageController = PageController(
-    initialPage: NewsTabLocation.globalNews.value,
-  );
-
-  void setNewsCurrentPage({required NewsTabLocation selectedPage}) {
-    newsCurrentPage = selectedPage;
-    newsPageController.animateToPage(
-      selectedPage.value,
-      duration: const Duration(milliseconds: 300),
-      curve: Curves.linearToEaseOut,
-    );
-    notifyListeners();
-  }
-
-  NewsGlobal? newsSelected;
-  bool newsSelectedIsSubject = false;
-
-  void setNewsSelected({
-    NewsGlobal? news,
-    bool isNewsSubject = false,
-  }) {
-    newsSelected = news;
-    newsSelectedIsSubject = isNewsSubject;
-    notifyListeners();
-  }
-  // End news page
 
   // Account page
   Map<String, Object> accountParameter = {};
